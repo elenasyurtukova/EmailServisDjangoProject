3d714dd19fe174355f0ec240223f2df5d7420f9d
Elena_Syurtukova <evasineva@bk.ru>
2025-08-14 22:07:38 +0900

    users app

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('emailservisapp.urls', namespace='emailservisapp')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('emailservisapp.urls', namespace='emailservisapp')),
+                  path('users/', include('users.urls', namespace='users')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
