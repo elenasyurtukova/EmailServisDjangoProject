f1dca389c2a898a9ac89c4283ac4e53495ef9362
Elena_Syurtukova <evasineva@bk.ru>
2025-08-16 20:50:19 +0900

    new password

--- config/settings.py
+++ config/settings.py
@@ -101,6 +101,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = 'users:login'
+
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
 from users.apps import UsersConfig
 from users.views import UserCreateView, email_varification
 
@@ -8,6 +9,21 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-reset/', PasswordResetView.as_view(
+        template_name="users/password_reset_form.html",
+        email_template_name="users/password_reset_email.html",
+        success_url=reverse_lazy("users:password_reset_done")),
+         name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+         name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name="users/password_reset_confirm.html",
+             success_url=reverse_lazy("users:password_reset_complete")),
+         name='password_reset_confirm'),
+    path('password-reset/complete/',
+         PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+         name='password_reset_complete'),
     path('register/', UserCreateView.as_view(), name='register'),
     path('email-confirm/<str:token>/', email_varification, name='email-confirm')
 ]
